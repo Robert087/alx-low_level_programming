3637e7c257ce0c617200444c41b4f82677a4a3bb
Robert087 <rotaitoro@gmail.com>
2023-07-21 07:31:48 -0700

    a

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,17 +4,11 @@
 * @c: Variable text
 * Return: Always 0.
 */
+
 int _isupper(int c)
 {
-
-
- if (c >= 'A' && c <= 'Z')
- {
- return (1);
- }
- else
-{
- return (0);
-}
-
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
 }
